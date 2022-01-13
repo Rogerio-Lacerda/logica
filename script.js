9acafb6abac1a219405952fdfaa393a2d43108dd
Rogerio-Lacerda <rogerio.barrosg@gmail.com>
2022-01-13 18:33:52 -0300

    Desafio básico de lógica de programação.

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "chocolate";
+
+let number = 12;
+
+let year = 2022;
+
+let subtracao = year - number;
+
+console.log(subtracao);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+
+console.log(typeof quartaFeira);
